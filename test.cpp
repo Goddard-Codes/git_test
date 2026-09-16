d144b00b4154dff6ca1b08a97958291bfc6fcbd2
Evan <evan_goddard11@yahoo.com>
2026-09-16 17:02:52 -0500

    Test for SD class

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  short day, month, year, format;
+
+  // Request day of the month (as a number) and put in day variable
+  cout << "Enter a day of the month (1, 12, 30, etc): ";
+  cin >> day;
+
+  //TODO: add the request for the month number and read it in
+
+  //TODO: add the request for the year and read it in
+
+  // Ask what format the user would like to see the date in
+  cout << "Formats are (1) YYYY/MM/DD, (2) DD/MM/YYYY, and (3) MM/DD/YYYY" << endl;
+  cout << "Enter the number for the format you want (1, 2, or 3): ";
+  cin >> format;
+
+  /*TODO: write the if statement that will use the value in format to
+        print out the date in the correct format. */
+}
